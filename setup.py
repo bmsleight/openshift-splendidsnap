9a270ff0d30572cc6a4877e4715308d71cc43aba
bmsleight <bms(#a#)barwap.com>
2016-01-15 00:42:31 +0000

    Adding install required to setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='SplendidSnap',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
-    description='OpenShift App',
+    description='Splendid Snap App',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='B Sleight',
     # GETTING-STARTED: set author email (your email):
     author_email='example@example.com',
     # GETTING-STARTED: set author url (your url):
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.8.4'
+        'Django==1.8.4',
+        'django-bootstrap3'
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
