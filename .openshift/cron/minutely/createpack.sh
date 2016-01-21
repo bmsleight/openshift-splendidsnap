bdf60030af5f5a7d414b3e428d365d4510c9a31b
bmsleight <bms(#a#)barwap.com>
2016-01-21 22:20:29 +0000

    chmod +x  .... helps

--- .openshift/cron/minutely/createpack.sh
+++ .openshift/cron/minutely/createpack.sh
@@ -1,1 +1,3 @@
-flock -n $OPENSHIFT_REPO_DIR/createpack.lock -c python $OPENSHIFT_DEPLOYMENTS_DIR/current/repo/wsgi/myproject/manage.py createpack
+#! /bin/bash
+date   > $OPENSHIFT_DATA_DIR/last_date_cron_ran
+flock -n $OPENSHIFT_DATA_DIR/createpack.lock -c python $OPENSHIFT_DEPLOYMENTS_DIR/current/repo/wsgi/myproject/manage.py createpack
