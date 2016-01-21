d60f7fed4aecaa71c05c219efbaa291987189d58
bmsleight <bms(#a#)barwap.com>
2016-01-21 22:40:11 +0000

    Getting the hang of cron

--- .openshift/cron/minutely/createpack.sh
+++ .openshift/cron/minutely/createpack.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-date   > $OPENSHIFT_DATA_DIR/last_date_cron_ran
-flock -n $OPENSHIFT_DATA_DIR/createpack.lock -c python $OPENSHIFT_DEPLOYMENTS_DIR/current/repo/wsgi/myproject/manage.py createpack
+flock -n $OPENSHIFT_DATA_DIR/createpack.lock -c "python $OPENSHIFT_DEPLOYMENTS_DIR/current/repo/wsgi/myproject/manage.py createpack"
