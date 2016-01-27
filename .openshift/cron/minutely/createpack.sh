360576882161c1b2ab71e9261efb0c86527052bb
bmsleight <bms(#a#)barwap.com>
2016-01-27 23:47:01 +0000

    nohup cron

--- .openshift/cron/minutely/createpack.sh
+++ .openshift/cron/minutely/createpack.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-flock -n $OPENSHIFT_DATA_DIR/createpack.lock -c "python $OPENSHIFT_DEPLOYMENTS_DIR/current/repo/wsgi/myproject/manage.py createpack"
+nohup flock -n $OPENSHIFT_DATA_DIR/createpack.lock -c "python $OPENSHIFT_DEPLOYMENTS_DIR/current/repo/wsgi/myproject/manage.py createpack" > $OPENSHIFT_LOG_DIR/cron_logfile 2>&1 &
